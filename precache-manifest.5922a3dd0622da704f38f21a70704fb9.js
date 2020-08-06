7706dac0b45f2ee28dc84e683834c708179f41a0
Albert Tan <alberthtan@Alberts-MacBook-Air.local>
2020-08-06 09:51:15 -0700

    Updates

---
+++ precache-manifest.5922a3dd0622da704f38f21a70704fb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a90eb186d604edb1510f9ab0b589294",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "13cb4da978c15aabf6dd",
+    "url": "/deploy-react/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "529f89c8aba98df8f2b6",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13cb4da978c15aabf6dd",
+    "url": "/deploy-react/static/js/2.989a5cbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.989a5cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529f89c8aba98df8f2b6",
+    "url": "/deploy-react/static/js/main.97c4c9f9.chunk.js"
+  },
+  {
+    "revision": "4ea2c2c7163b2a9c9a35",
+    "url": "/deploy-react/static/js/runtime-main.095c1313.js"
+  }
+]);
